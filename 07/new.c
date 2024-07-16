3c0050b2ed1ebabb888262e6004c577625892185
FIXED-TERM PENG Yiyang (RBCW/ENG-PC) <pyg1wx@bosch.com>
2024-07-16 14:42:11 +0800

    update2

---
+++ 07/new.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main(){
+    if(argc!=2){
+        printf("usage: %s <file> \n",argv[0])
+        return -1;
+    }
+    int fd=open(argv[1],O_RDONLY);
+    int fd2 = open(argv[1], O_RDONLY);
+    printf("fd = %d\n", fd);
+    printf("fd2 = %d\n", fd2);
+    while(1){
+        sleep(100);
+    }
+}
