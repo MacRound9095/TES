864d930cb05d86e3690f6978d5c230065a8300d3
MacRound9095 <350264217@qq.com>
2024-07-19 15:16:54 +0800

    update6

--- thread/t1_copy.c
+++ thread/t1_copy.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <semaphore.h>
-
+#include<string.h>
 // 互斥量：在子线程打印buf内容的时候，如果此时继续输入键盘，可能会导致打印的内容前半部分是之前的，后半部分是刚输入的
 // 因此需要定义互斥量，保证子线程在打印内容的时候，主线程不会输入数据到buf
 // 用法：一般主要将访问公共变量资源的操作前后加锁，确保在一段时间内，只能有一个线程访问该资源，在这里就是把BUF锁起来
 
 static char global_buf[999];
+static char temp_buf[999];
 static sem_t isem;
 static pthread_mutex_t g_tmutex = PTHREAD_MUTEX_INITIALIZER;
 static void *my_func (void *data){
@@ -33,8 +34,9 @@ int main(int apsrgc,char **argv){
     }
     // 2 主线程读取标准输入，发给接收线程
     while(1){
+            fgets(temp_buf, 999, stdin);
             pthread_mutex_lock(&g_tmutex);
-           fgets(global_buf, 999, stdin);
+            memcpy(global_buf, temp_buf, size_t n);
             pthread_mutex_unlock(&g_tmutex);
            sem_post(&isem);
     }
