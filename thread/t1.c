4f114ee6c14787d57b1052c675c893ebb4cf2b4b
MacRound9095 <350264217@qq.com>
2024-07-19 14:54:05 +0800

    update6

--- thread/t1.c
+++ thread/t1.c
@@ -2,16 +2,21 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <semaphore.h>
 
+static char global_buf[999];
+static sem_t isem;
 static void *my_func (void *data){
     while(1){
-        sleep(10);
+        sem_wait(&isem);
+        printf("the message are %s \n",global_buf)
     }
 }
 
 int main(int apsrgc,char **argv){
     pthread_t tid;
     int ret;
+    sem_init(&isem, 0, 0);
     // 1 创建接收线程
     ret=pthread_create(&tid, NULL,my_func,NULL);
     if(ret){
@@ -20,7 +25,8 @@ int main(int apsrgc,char **argv){
     }
     // 2 主线程读取标准输入，发给接收线程
     while(1){
-        sleep(10);
+           fgets(global_buf, 999, FILE *stream);
+           sem_post(&isem);
     }
     return 0;
 
